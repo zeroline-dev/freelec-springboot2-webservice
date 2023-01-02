d57b75f29075f1e2a9a2a83e4ebf6995f93d65aa
zero <yuioi0113@daum.net>
2023-01-02 17:48:49 +0900

    머스테치로 화면 구성하기 - 전체조회화면 생성완료 p.150

--- src/main/java/com/zeroline/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/zeroline/book/springboot/service/posts/PostsService.java
@@ -3,18 +3,22 @@ package com.zeroline.book.springboot.service.posts;
 
 import com.zeroline.book.springboot.domain.posts.Posts;
 import com.zeroline.book.springboot.domain.posts.PostsRepository;
+import com.zeroline.book.springboot.web.dto.PostsListResponseDto;
 import com.zeroline.book.springboot.web.dto.PostsResponseDto;
 import com.zeroline.book.springboot.web.dto.PostsSaveRequestDto;
 import com.zeroline.book.springboot.web.dto.PostsUpdateReqeustDto;
 import lombok.RequiredArgsConstructor;
+import net.bytebuddy.asm.Advice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RequiredArgsConstructor
 @Service
 public class PostsService {
-
     private final PostsRepository postsRepository;
 
     @Transactional
@@ -37,6 +41,13 @@ public class PostsService {
                 IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
 
---
+++ src/main/java/com/zeroline/book/springboot/web/IndexController.java
@@ -0,0 +1,26 @@
+package com.zeroline.book.springboot.web;
+
+import com.zeroline.book.springboot.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+    private final PostsService postsService;
+
+    @GetMapping("/")
+    public String Index(Model model) {
+        model.addAttribute("posts",postsService.findAllDesc());
+        return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave(){
+        return "posts-save";
+    }
+
+}
